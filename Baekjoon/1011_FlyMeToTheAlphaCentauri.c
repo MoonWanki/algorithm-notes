6c1457248c9916cbeba64304530777293db457bf
MoonWanki <dhksrl2589@gmail.com>
2018-12-20 22:42:13 +0900

    Backjoon #1011

---
+++ Baekjoon/1011_FlyMeToTheAlphaCentauri.c
@@ -0,0 +1,26 @@
+// BACKJOON #1011 <Fly me to the Alpha Centrauri>
+// https://www.acmicpc.net/problem/1011
+
+#include <stdio.h>
+
+unsigned int arr[150000], t, from, to, val = 1, term = 1;
+
+int getMinTimes(unsigned int distance) {
+    for(int i=1 ; i<140000 ; i++) {
+        if(arr[i] > distance) return i-1;
+    }
+}
+
+int main() {
+    for(int i=1 ; i<140000; i+=2) {
+        arr[i] = val;
+        val += term;
+        arr[i+1] = val;
+        val += term;
+        term++;
+    }
+    for(scanf("%d", &t) ; t-->0 ;) {
+        scanf("%d %d", &from, &to);
+        printf("%d\n", getMinTimes(to - from));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# algorithm-notes
-Baekjoon, Programmers, etc.
+# Algorithm notes
+
+Coding exercise with variable algorithms
+
+## Compile
+
+- C
+```
+gcc test.c -o test.exe
+```
+
+- Java
+```
+(will be updated soon)
+```
+
+- Python 3
+```
+(will be updated soon)
+```
+
+## References
+- Backjoon Online Judge  
+https://www.acmicpc.net  
+
+- Programmers  
+https://programmers.co.kr/  
